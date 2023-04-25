aee2835ad510dc21eee033d0d54d1fec33c3b3d1
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2023-04-24 22:14:13 -0300

    adds mainnet addrs

--- src/common/const.ts
+++ src/common/const.ts
@@ -32,7 +32,10 @@ export const CIRCLE_CONTRACT_ADDRESSES: AddressesByEnvAndChain = {
     [CHAIN_ID_ETH]: "0x26413e8157cd32011e726065a5462e97dd4d03d9",
     [CHAIN_ID_AVAX]: "0xa9fb1b3009dcb79e2fe346c16a604b8fa8ae0a79",
   },
-  [Environment.MAINNET]: {},
+  [Environment.MAINNET]: {
+    [CHAIN_ID_ETH]: "0x0a992d191DEeC32aFe36203Ad87D7d289a738F81",
+    [CHAIN_ID_AVAX]: "0x8186359af5f57fbb40c6b14a588d2a59c0c29880",
+  },
   [Environment.DEVNET]: {},
 };
 
@@ -43,8 +46,8 @@ export const USDC_RELAYER_ADDRESSES: AddressesByEnvAndChain = {
     [CHAIN_ID_AVAX]: "0xb9f955b03cea9315247e77a09b6e2f1c587e017f",
   },
   [Environment.MAINNET]: {
-    [CHAIN_ID_ETH]: "",
-    [CHAIN_ID_AVAX]: "",
+    [CHAIN_ID_ETH]: "0x32dec3f4a0723ce02232f87e8772024e0c86d834",
+    [CHAIN_ID_AVAX]: "0x32dec3f4a0723ce02232f87e8772024e0c86d834",
   },
   [Environment.DEVNET]: {
     [CHAIN_ID_ETH]: "",
@@ -59,8 +62,8 @@ export const USDC_WH_SENDER: AddressesByEnvAndChain = {
     [CHAIN_ID_AVAX]: "0x58f4c17449c90665891c42e14d34aae7a26a472e",
   },
   [Environment.MAINNET]: {
-    [CHAIN_ID_ETH]: "",
-    [CHAIN_ID_AVAX]: "",
+    [CHAIN_ID_ETH]: "0xaada05bd399372f0b0463744c09113c137636f6a",
+    [CHAIN_ID_AVAX]: "0x09fb06a271faff70a651047395aaeb6265265f13",
   },
   [Environment.DEVNET]: {
     [CHAIN_ID_ETH]: "",
