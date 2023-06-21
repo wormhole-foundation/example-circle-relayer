cc6cac09eeeee933ed5b99b41802fdf5a02fdf20
gator-boi <gator-boi@users.noreply.github.com>
2023-06-21 14:52:29 +0000

    evm: fix typeschain

--- evm/shell-scripts/make_ethers_types.sh
+++ evm/shell-scripts/make_ethers_types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SRC=$(dirname $0)/../out
-DST=$(dirname $0)/../ts-test/src/ethers-contracts
+DST=$(dirname $0)/../ts/src/ethers-contracts
 
 typechain --target=ethers-v5 --out-dir=$DST $SRC/*/*.json
