0b69f1d81cff27a9512d665891eb022c1808bbc6
Abhishek Rajput <abhi@qwestive.io>
2023-11-09 19:38:20 +0530

    Update repo name in script

--- src/build-image.sh
+++ src/build-image.sh
@@ -15,8 +15,8 @@ else
 fi
 set -e
 aws ecr get-login-password --region us-east-2 --profile $AWS_PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com
-docker build --platform=linux/amd64 -t $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/cctp-relayer:$TAG --secret id=npmrc,src=$HOME/.npmrc .
-docker push $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/cctp-relayer:$TAG
+docker build --platform=linux/amd64 -t $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/circle-relayer:$TAG --secret id=npmrc,src=$HOME/.npmrc .
+docker push $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/circle-relayer:$TAG
 
 set +e
 
